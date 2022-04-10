e76805376c48b3db895908d363097c747b43b787
Akshayanand <50578479+AT0M1CK@users.noreply.github.com>
2022-04-10 17:47:41 +0530

    refactor: Basic Mysql Connection Testing

---
+++ src/api/mysql-adapter.js
@@ -0,0 +1,32 @@
+const mysql = require("mysql");
+
+class DatabaseAdapter {
+  constructor() {
+    this.connection = null;
+  }
+
+  connect() {
+    this.connection = mysql.createConnection({
+      host: "localhost",
+      user: "studentman",
+      password: "study123",
+      database: "StudentManagement",
+    });
+
+    this.connection.connect((err) => {
+      if (err) throw err;
+      console.log("Connected to MySQL Server!");
+    });
+  }
+
+  query(queryString) {
+    this.connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      return result;
+    });
+  }
+}
+
+module.exports = DatabaseAdapter;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,36 @@
 // main index file
 
 var express = require("express");
-
+const DatabaseAdapter = require("./api/mysql-adapter");
+var database = require("./api/mysql-adapter");
 var app = express();
 
-let students = [
-    {
-        name:"raj",
-        age:12,
-    },
-    {
-        name:"ram",
-        age:16,
-    },
-    {
-        name:"bheem",
-        age:13,
-    },
-    {
-        name:"arjun",
-        age:14,
-    },
-    {
-        name:"seeta",
-        age:15,
-    }
-]
+//Database talbes
+const userTable = "tbl_users";
 
+app.get("/", function (req, res) {
+  res.send("You are on the root of server");
+});
 
+app.get("/register", function (req, res) {
+  console.log(req.query);
+  const { username, email, password } = req.query;
+  const result = registerUser(username, email, password);
+  res.send("ok", result);
+});
 
-app.get('/',function(req,res){
-    res.send("You are on the root of server");
-})
+var server = app.listen(8000, function () {
+  var host = server.address().address;
+  var port = server.address.port;
+  console.log("Starting server...");
+});
 
-app.get('/students',function(req,res){
-    res.send(students);
-})
+function registerUser(username, email, password) {
+  const adapter = new DatabaseAdapter();
+  adapter.connect();
 
+  const queryString = `INSERT INTO ${userTable} (username, email, password) VALUES ('${username}','${email}','${password}')`;
+  const result = adapter.query(queryString);
 
-var server = app.listen(8000,function(){
-    var host = server.address().address;
-    var port = server.address.port;
-    console.log("Starting server...");
-})
-
+  return result;
+}
