45986e748f41ec8bd5f5b38a4670408d4dba1616
Akshayanand <50578479+AT0M1CK@users.noreply.github.com>
2022-04-16 15:07:57 +0530

    refactor: Added student editing

--- src/routes/edit-student.js
+++ src/routes/edit-student.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const connection = require("../database/mysql-connector");
 
-router.get("/", function (req, res) {
-    console.log(req.query);
-   
-    res.status(200).send("student route");
+const userTable = "tbl_users";
+
+router.post("/", function (req, res) {
+  console.log(req.query);
+
+  const { id, Grade } = req.body;
+  console.log(req.body);
+
+  const queryString = `UPDATE ${userTable} SET Grade = '${Grade}' WHERE id = '${id}'`;
+
+  connection.query(queryString, function (err, result) {
+    if (err) {
+      throw err;
+    }
+    res.status(200).send({
+      message: `User ${id} updated`,
+    });
   });
+});
 
-  module.exports = router;
+module.exports = router;
