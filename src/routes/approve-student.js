376b44da8470869e7e8a3f14778e32c719d60a7e
Akshayanand <50578479+AT0M1CK@users.noreply.github.com>
2022-04-16 14:32:50 +0530

    refactor: Added student approval

--- src/routes/approve-student.js
+++ src/routes/approve-student.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const connection = require("../database/mysql-connector");
 
-router.get("/", function (req, res) {
-    console.log(req.query);
-   
-    res.status(200).send("admin route");
+const userTable = "tbl_users";
+
+router.post("/", function (req, res) {
+  console.log(req.query);
+
+  const { id } = req.body;
+
+  const queryString = `UPDATE ${userTable} SET status = 'approved' WHERE id = '${id}'`;
+
+  connection.query(queryString, function (err, result) {
+    if (err) {
+      throw err;
+    }
+    res.status(200).send({
+      message: `UserID ${id} approved`,
+    });
   });
+});
 
-  module.exports = router;
+module.exports = router;
